33375f2aadb747abc8d783abb7e44877b84d2ff8
Ho-no <Hono_sama@qq.com>
2022-05-25 14:24:51 +0800

    Complete first fragment

--- app/src/main/java/com/example/assignment1/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/assignment1/ui/home/HomeFragment.kt
@@ -1,9 +1,14 @@
 package com.example.assignment1.ui.home
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -33,9 +38,59 @@ class HomeFragment : Fragment() {
         val editTextAge: EditText = binding.editTextHomeAge
         val editTextHeight: EditText = binding.editTextHomeHeight
 
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+        val textView : TextView = binding.textHome
+
+        val buttonDisplay: Button = binding.displayButtonHome
+        val buttonDelete: Button = binding.deleteButtonHome
+
+        textView.visibility = INVISIBLE
+
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+        editTextAge.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s:CharSequence, start:Int, count:Int, after:Int) {
+
+            }
+            override fun onTextChanged(s:CharSequence, start:Int, count:Int, after:Int) {
+
+            }
+            override fun afterTextChanged(s: Editable?) {
+                val content = s.toString().toInt()
+                homeViewModel.updateAge(content)
+            }
+        })
+
+        editTextName.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s:CharSequence, start:Int, count:Int, after:Int) {
+
+            }
+            override fun onTextChanged(s:CharSequence, start:Int, count:Int, after:Int) {
+
+            }
+            override fun afterTextChanged(s: Editable?) {
+                val content = s.toString()
+                homeViewModel.updateName(content)
+            }
+        })
+
+        editTextHeight.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s:CharSequence, start:Int, count:Int, after:Int) {
+
+            }
+            override fun onTextChanged(s:CharSequence, start:Int, count:Int, after:Int) {
+
+            }
+            override fun afterTextChanged(s: Editable?) {
+                val content = s.toString().toDouble()
+                homeViewModel.updateHeight(content)
+            }
+        })
+
+        buttonDisplay.setOnClickListener {textView.visibility = VISIBLE}
+        buttonDelete.setOnClickListener {textView.visibility = INVISIBLE}
+
         return root
     }
 
--- app/src/main/java/com/example/assignment1/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/assignment1/ui/home/HomeViewModel.kt
@@ -6,9 +6,39 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
-    private val age: Int = 0
+    private val _age: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>().also { it.value = 0 }
+    }
+
+    private val _name: MutableLiveData<String> by lazy {
+        MutableLiveData<String>().also { it.value = "" }
+    }
+
+    private val _height: MutableLiveData<Double> by lazy {
+        MutableLiveData<Double>().also { it.value = 0.0 }
+    }
+
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = ""
     }
     val text: LiveData<String> = _text
+
+    private fun updateText() {
+        _text.value =  "${_name.value}${_age.value.toString()}岁了，身高是${_height.value}米"
+    }
+
+    fun updateAge(age: Int) {
+        _age.value = age
+        updateText()
+    }
+
+    fun updateName(name: String) {
+        _name.value = name
+        updateText()
+    }
+
+    fun updateHeight(height: Double) {
+        _height.value = height
+        updateText()
+    }
 }
